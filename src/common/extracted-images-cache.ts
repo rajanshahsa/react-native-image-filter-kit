0d299b5576a9071fb6e128f321caebcce46f7496
Rajan Shah <rajan.shah@solutionanalysts.com>
2019-12-06 19:03:07 +0530

    [fix] IFKExtractedImagesCache crash in android

--- src/common/extracted-images-cache.ts
+++ src/common/extracted-images-cache.ts
@@ -2,4 +2,4 @@ import { NativeModules } from 'react-native'
 
 const { IFKExtractedImagesCache } = NativeModules
 
-export const cleanExtractedImagesCache = IFKExtractedImagesCache.clean
+export const cleanExtractedImagesCache = IFKExtractedImagesCache? IFKExtractedImagesCache.clean : null
